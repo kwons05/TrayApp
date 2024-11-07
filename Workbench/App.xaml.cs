8d93a3c53bffbca897cebb16ba648af412590bbb
kwonsss <sh.kwon@dhandy.co.kr>
2024-11-07 14:23:17 +0900

    init

---
+++ Workbench/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System.Windows;
+
+namespace Workbench
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private TaskbarIcon notifyIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //create the notifyicon (it's a resource declared in NotifyIconResources.xaml
+            notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            notifyIcon.Dispose(); //the icon would clean up automatically, but this is cleaner
+            base.OnExit(e);
+        }
+    }
+
+}
---
+++ Workbench/NotifyIconViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Workbench
+{
+    public class NotifyIconViewModel
+    {
+        private MainWindow GetMainWindow()
+        {
+            return Application.Current.MainWindow as MainWindow;
+        }
+
+        /// <summary>
+        /// Shows a window, if none is already open.
+        /// </summary>
+        public ICommand ShowWindowCommand
+        {
+            get
+            {
+                return new DelegateCommand
+                {
+                    CanExecuteFunc = () => this.GetMainWindow() == null,
+                    CommandAction = () =>
+                    {
+                        Application.Current.MainWindow = new MainWindow();
+                        Application.Current.MainWindow.Show();
+                    }
+                };
+            }
+        }
+
+        /// <summary>
+        /// Hides the main window. This command is only enabled if a window is open.
+        /// </summary>
+        public ICommand HideWindowCommand
+        {
+            get
+            {
+                return new DelegateCommand
+                {
+                    CommandAction = () => this.GetMainWindow()?.Close(),
+                    CanExecuteFunc = () => this.GetMainWindow() != null
+                };
+            }
+        }
+
+
+        /// <summary>
+        /// Shuts down the application.
+        /// </summary>
+        public ICommand ExitApplicationCommand
+        {
+            get { return new DelegateCommand { CommandAction = () => Application.Current.Shutdown() }; }
+        }
+    }
+}
